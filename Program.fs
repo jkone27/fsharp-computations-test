8462b6539d18af6f40225b95e96ca01598c7911b
jkone27 <jkone27@yahoo.it>
2019-04-06 14:18:53 +0200

    added some tests

---
+++ Program.fs
@@ -0,0 +1,42 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Types
+
+type Test(s, validationFunction) = 
+    member val Property : string = s with get, set
+    new() =
+        Test("", fun _ -> true)
+    new(str) = Test(str, fun _ -> true)
+    interface IBuild<string,Test> with 
+        member __.Build x validationFunction = Test(x, validationFunction)
+        member __.IsValid = validationFunction(__.Property)
+
+let testBuilder = GenericBuilder<string,Test>(fun x -> x.Contains("true"))
+
+let log = SimpleDoMonad<int>(fun x -> Console.WriteLine(x))
+
+[<EntryPoint>]
+let main argv =
+
+    let r = maybe {
+        let x = Some 1
+        let! y = x
+        return 1
+    }
+
+    let x = testBuilder {
+        let x = new Test("hello")
+        let! x1 = x
+        let r = x1.Property
+        return r
+    }
+
+    let r = log {
+        let! x = 1 //logs
+        let y = 2 //doesnt
+        return (x + y) //logs
+    }
+
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ Types.fs
@@ -0,0 +1,32 @@
+﻿module Types
+
+type IBuild<'a,'T> =
+    abstract member Build : 'a -> ('a -> bool) -> 'T
+    abstract member IsValid : bool with get
+    
+//non-sense crazyness
+type GenericBuilder<'a, 'T  when 'T :> IBuild<'a,'T> and 'T : (new : unit -> 'T)>(validatingFunc) =
+    member __.Return(x: 'a) = (new 'T() :> IBuild<'a,'T>).Build x validatingFunc
+    member __.Delay(f) = f()
+    member __.Bind(x,f) = f(x)
+
+type MaybeBuilder() =
+    member __.Bind(x, f) =
+        match x with
+        | Some(x) -> f(x)
+        | _ -> None
+    member __.Delay(f) = f()
+    member __.Return(x) = Some x
+
+type SimpleDoMonad<'T>(someSideEffect: 'T -> unit) =
+    member __.Bind(x,f) = 
+        let _ = __.Return(x)
+        f(x)
+    member __.Return(x) =
+        someSideEffect(x) |> ignore
+        x
+    
+
+let maybe = MaybeBuilder()
+
+
